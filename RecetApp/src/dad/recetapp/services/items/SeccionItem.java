bc6a17c020f78e2fca73ac33136bb92f52d3eb86
portaslua <osrcorreo@hotmail.com>
2015-01-29 10:51:46 +0000

    equals de SeccionItem

--- RecetApp/src/dad/recetapp/services/items/SeccionItem.java
+++ RecetApp/src/dad/recetapp/services/items/SeccionItem.java
@@ -6,38 +6,46 @@ import java.util.List;
 import dad.recetapp.services.IItem;
 
 public class SeccionItem implements IItem {
-    private Long id;
-    private String nombre;
-    private List<IngredienteItem> ingredientes = new ArrayList<IngredienteItem>();
-    private List<InstruccionItem> instrucciones = new ArrayList<InstruccionItem>();
+	private Long id;
+	private String nombre;
+	private List<IngredienteItem> ingredientes = new ArrayList<IngredienteItem>();
+	private List<InstruccionItem> instrucciones = new ArrayList<InstruccionItem>();
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public List<IngredienteItem> getIngredientes() {
-        return ingredientes;
-    }
+	public List<IngredienteItem> getIngredientes() {
+		return ingredientes;
+	}
 
-    public List<InstruccionItem> getInstrucciones() {
-        return instrucciones;
-    }
+	public List<InstruccionItem> getInstrucciones() {
+		return instrucciones;
+	}
 
-    @Override
-    public String toString() {
-        return nombre;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (o != null && o instanceof SeccionItem && this.id == ((SeccionItem)o).getId())
+			return true;
+		else
+			return false;
+	}
+
+	@Override
+	public String toString() {
+		return nombre;
+	}
 
 }
